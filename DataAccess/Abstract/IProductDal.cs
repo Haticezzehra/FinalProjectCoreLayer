0c529cdcd28f69be4155c1094c307e1ae53643cf
example <haticezehraorhan@gmail.com>
2023-01-19 13:19:04 +0300

    Dal

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll();//Herşeyi getirecek.
+        void Add(Product product);
+        void Upgrade(Product product);  
+        void Delete(Product product);   
+            }
+}
