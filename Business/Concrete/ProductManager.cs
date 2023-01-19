088a93d92b51c9d4f912194009a08ea72b35045f
example <haticezehraorhan@gmail.com>
2023-01-19 15:43:43 +0300

    DI

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal productDal;//DependencyInjection
+
+        public ProductManager(IProductDal productDal)
+        {
+            this.productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+           return productDal.GetAll();   
+        }
+    }
+}
