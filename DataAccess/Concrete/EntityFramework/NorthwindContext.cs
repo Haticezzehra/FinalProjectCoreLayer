56fb06bf741c17ff6e0da127d58889397570199a
example <haticezehraorhan@gmail.com>
2023-01-22 21:23:57 +0300

    db

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    internal class NorthwindContext
-    {
+    //Context: db tabloları ile proje claslarını bağlamak.
+    //Böylece sınıfımıza sen bir db contexin demiş olduk.
+    public class NorthwindContext:DbContext //EntityFrameworkden gelen bir class.
+    { 
+        //Projemiz hangi veritabanı ile ilişkili olduğunu belirttiğimiz yer
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; } //Veritabanında hangi tablo hangi nesneye denk geliyo böyle belirliyoruz.
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Customer> Customers { get; set; }
     }
 }
