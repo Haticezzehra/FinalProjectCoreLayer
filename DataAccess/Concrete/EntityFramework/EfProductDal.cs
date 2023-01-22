88b7acd84bf6e6205c6130a1df5941106a8999f7
example <haticezehraorhan@gmail.com>
2023-01-22 22:41:56 +0300

    n

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +14,26 @@ namespace DataAccess.Concrete.EntityFramework
     {
         public void Add(Product entity)
         {
-            throw new NotImplementedException();
+            // using kullanmayı bitirince GarbageCollector kaldırıyo bellekten.DirekNorthwindide newlwywbilirdik ama o
+            //zaman belleğe çok yüklenmiş oluyoruz. Yani Northwind bellekten işi bitince atılıyo.
+            //IDısposable pattern implementation pf C#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity=context.Entry(entity);//Veri kaynağımla entity iilişkilendir. Referansını bul
+                addedEntity.State = EntityState.Added; //Ekle
+                context.SaveChanges(); // Burdaki işlemleri gerçekleştir.
+
+            }
         }
 
         public void Delete(Product entity)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity= context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public Product Get(Expression<Func<Product, bool>> filter = null)
@@ -38,7 +53,12 @@ namespace DataAccess.Concrete.EntityFramework
 
         public void Update(Product entity)
         {
-            throw new NotImplementedException();
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 }
