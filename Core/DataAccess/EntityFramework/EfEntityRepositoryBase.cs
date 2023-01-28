c5b80706ba5539642cfc3bf5d78012793750c986
example <haticezehraorhan@gmail.com>
2023-01-28 23:54:20 +0300

    core

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -15,27 +15,60 @@ where TContext : DbContext, new()
     {
         public void Add(TEntity entity)
         {
-            throw new NotImplementedException();
+            // using kullanmayı bitirince GarbageCollector kaldırıyo bellekten.DirekNorthwindide newlwywbilirdik ama o
+            //zaman belleğe çok yüklenmiş oluyoruz. Yani Northwind bellekten işi bitince atılıyo.
+            //IDısposable pattern implementation pf C#
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);//Veri kaynağımla entity iilişkilendir. Referansını bul
+                addedEntity.State = EntityState.Added; //Ekle
+                context.SaveChanges(); // Burdaki işlemleri gerçekleştir.
+
+            }
         }
 
         public void Delete(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
         }
 
         public TEntity Get(Expression<Func<TEntity, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
         }
 
         public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null ?
+                    context.Set<TEntity>().ToList()
+                    :
+                    context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public List<TEntity> GetAllByCategory(int categoryId)
         {
             throw new NotImplementedException();
         }
 
         public void Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
         }
     }
 }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -12,62 +12,6 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfProductDal : IProductDal
     {
-        public void Add(Product entity)
-        {
-            // using kullanmayı bitirince GarbageCollector kaldırıyo bellekten.DirekNorthwindide newlwywbilirdik ama o
-            //zaman belleğe çok yüklenmiş oluyoruz. Yani Northwind bellekten işi bitince atılıyo.
-            //IDısposable pattern implementation pf C#
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var addedEntity=context.Entry(entity);//Veri kaynağımla entity iilişkilendir. Referansını bul
-                addedEntity.State = EntityState.Added; //Ekle
-                context.SaveChanges(); // Burdaki işlemleri gerçekleştir.
-
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var deletedEntity= context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter == null ?
-                    context.Set<Product>().ToList()
-                    :
-                    context.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public List<Product> GetAllByCategory(int categoryId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
+       
     }
 }
